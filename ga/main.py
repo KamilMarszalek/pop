3ff41c9f42a94e0ad3be85ce3e15a93aaed2c656
KamilMarszalek <kamilmar553@gmail.com>
2025-11-02 19:16:33 +0100

    change crossover operation each individual can be chosen multiple times

--- ga/main.py
+++ ga/main.py
@@ -148,20 +148,17 @@ def crossover(
     probability_of_crossover: float,
     population_count: int,
 ) -> Population:
-    population_copy = deepcopy(population)
     new_population = []
     while len(new_population) < population_count:
-        unit1 = choice(population_copy)
-        population_copy.remove(unit1)
-        unit2 = choice(population_copy)
-        population_copy.remove(unit2)
+        unit1 = choice(population)
+        unit2 = choice(population)
         if uniform(0, 1) < probability_of_crossover:
             new_unit1, new_unit2 = unit1.cross(unit2)
             new_population.extend([new_unit1, new_unit2])
         else:
             new_population.extend([unit1, unit2])
 
-    return new_population
+    return new_population[:population_count]
 
 
 def mutation(
