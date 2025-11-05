6ae739a997d56b314181bfb137dc01aaa30951cd
Michał Szwejk <mszwejk1@gmail.com>
2025-11-05 22:51:11 +0100

    remove redundant tile struct

--- sa/board_state.py
+++ sa/board_state.py
@@ -1,59 +1,53 @@
-from dataclasses import dataclass
-from typing import Callable, override
+from typing import Callable
 
 from util.types import Board
 
 type HeuristicFunc = Callable[[Tile], int] | None
-
-
-@dataclass
-class Tile:
-    weight: int
-    row: int
-    column: int
-    is_occupied: bool
-
-    @override
-    def __hash__(self) -> int:
-        return hash((self.row, self.column))
+type Tile = tuple[int, int]
 
 
 class BoardState:
     def __init__(self, board: Board) -> None:
         self.n: int = len(board)
         self.m: int = len(board[0])
-        self.board: list[list[Tile]] = [
-            [Tile(board[i][j], i, j, False) for j in range(self.m)]
-            for i in range(self.n)
+        self.board: Board = board
+        self.selection_gird: list[list[bool]] = [
+            [False for _ in range(self.m)] for _ in range(self.n)
         ]
-        self.occupied_tiles: set[Tile] = set()
+        self.selected_tiles: set[Tile] = set()
 
     def evaluate_sum(self) -> int:
         return sum(
-            self.board[i][j].weight
+            self.board[i][j]
             for j in range(self.m)
             for i in range(self.n)
-            if self.board[i][j].is_occupied
+            if self.selection_gird[i][j]
         )
 
     def greedy_fill(self, max_cards: int, h_func: HeuristicFunc = None) -> None:
         while (tile := self._find_best_isolated_tile(h_func)) and len(
-            self.occupied_tiles
+            self.selected_tiles
         ) < max_cards:
-            self.occupied_tiles.add(tile)
-            tile.is_occupied = True
+            row, column = tile
+            self.selection_gird[row][column] = True
+            self.selected_tiles.add(tile)
 
     def can_tile_be_selected(self, tile: Tile) -> bool:
+        row, column = tile
         neighbors = self._neighbors(tile)
-        return not any(n.is_occupied for n in neighbors) and not tile.is_occupied
+        return (
+            not any(self.selection_gird[i][j] for i, j in neighbors)
+            and not self.selection_gird[row][column]
+        )
 
     def _neighbors(self, tile: Tile) -> list[Tile]:
+        row, column = tile
         result: list[Tile] = []
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for x, y in directions:
-            new_row, new_column = tile.row + x, tile.column + y
+            new_row, new_column = row + x, column + y
             if 0 <= new_row < self.n and 0 <= new_column < self.m:
-                result.append(self.board[new_row][new_column])
+                result.append((new_row, new_column))
         return result
 
     def _find_best_isolated_tile(self, h_func: HeuristicFunc = None) -> Tile | None:
@@ -61,7 +55,7 @@ class BoardState:
         best_score, best_tile = 0, None
         for i in range(self.n):
             for j in range(self.m):
-                tile = self.board[i][j]
+                tile = (i, j)
                 if not self.can_tile_be_selected(tile):
                     continue
                 score = scoring(tile)
@@ -71,4 +65,5 @@ class BoardState:
         return best_tile
 
     def _default_scoroing(self, tile: Tile) -> int:
-        return tile.weight // (1 + len(self._neighbors(tile)))
+        row, column = tile
+        return self.board[row][column] // (1 + len(self._neighbors(tile)))
