cdfde22cbb28ee7101d7de5afeb73046a2caa8a9
Michał Szwejk <mszwejk1@gmail.com>
2025-11-08 13:51:50 +0100

    top down solution fixed

--- src/dp/top_down.py
+++ src/dp/top_down.py
@@ -7,11 +7,11 @@ from src.util.util import (
     merge_compatibility,
 )
 
-type Memoization = dict[int, dict[int, tuple[int, list[int]]]]
+type Memoization = dict[int, dict[tuple[int, int], tuple[int, list[int]]]]
 
 
-def create_memoization(n_rows: int, possible_masks: list[int]) -> Memoization:
-    return {i: {mask: (0, []) for mask in possible_masks} for i in range(n_rows)}
+def create_memoization(n_rows: int) -> Memoization:
+    return {i: {} for i in range(n_rows)}
 
 
 def mwis_top_down(
@@ -23,15 +23,16 @@ def mwis_top_down(
     possible_masks = generate_non_adjacent_masks(len(board[0]))
     compatibility = get_masks_compatibility(possible_masks)
     masks_bit_count = get_masks_bit_count(possible_masks)
-    memo = create_memoization(len(board), possible_masks)
+    memo = create_memoization(len(board))
 
     def dsf(
         row_index: int, cards_used: int, previous_mask: int
     ) -> tuple[int, list[int]]:
-        if row_index == len(board) or cards_used == max_cards:
+        if row_index == len(board) or cards_used >= max_cards:
             return 0, []
-        max_sum, best_path = memo[row_index][previous_mask]
-        if max_sum == 0:
+        key = (previous_mask, cards_used)
+        if key not in memo[row_index]:
+            max_sum, best_path = 0, []
             comp = compatibility[previous_mask]
             if row_index == len(board) - 1:
                 comp = merge_compatibility(
@@ -40,15 +41,12 @@ def mwis_top_down(
             for mask in comp:
                 c = cards_used + masks_bit_count[mask]
                 if c <= max_cards:
-                    accumulated_sum, p = dsf(row_index + 1, c, mask)
-                    accumulated_sum = accumulated_sum + calculate_row_sum(
-                        board[row_index], mask
-                    )
-                    if accumulated_sum > max_sum:
-                        max_sum = accumulated_sum
+                    new_sum, p = dsf(row_index + 1, c, mask)
+                    new_sum += calculate_row_sum(board[row_index], mask)
+                    if new_sum > max_sum:
+                        max_sum = new_sum
                         best_path = [mask] + p
-
-            memo[row_index][previous_mask] = (max_sum, best_path)
-        return memo[row_index][previous_mask]
+            memo[row_index][key] = (max_sum, best_path)
+        return memo[row_index][key]
 
     return dsf(0, 0, initial_mask)
