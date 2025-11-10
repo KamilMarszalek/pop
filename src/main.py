a02e0af3fabd8aa2a1437068bd522932773040a8
KamilMarszalek <kamilmar553@gmail.com>
2025-11-10 18:59:57 +0100

    Refactor main function in main.py to adjust row count, streamline output, and enhance genetic algorithm integration

--- src/main.py
+++ src/main.py
@@ -4,6 +4,14 @@ from random import randint
 from src.astar.astar import AStar
 from src.dp.bottom_up import mwis_bottom_up
 from src.dp.top_down import mwis_top_down
+from src.ga import (
+    genetic_algorithm,
+    crossover,
+    mutation,
+    succession,
+    q,
+    reproduction,
+)
 from src.sa.greedy_and_repair import greedy_and_repair
 from src.util.types import Board
 
@@ -19,9 +27,9 @@ def generate_board(rows: int, columns: int, r: Range) -> Board:
 
 
 N_COLUMNS = 4
-N_ROWS = 400
+N_ROWS = 10
 LIMITS = Range(-10000, 10000)
-N_CARDS = (N_ROWS * N_COLUMNS) // 2
+N_CARDS = 5
 
 
 def main() -> None:
@@ -30,60 +38,47 @@ def main() -> None:
     print("DP Top down")
     result, path = mwis_top_down(board, N_CARDS)
     print(f"Result: {result}")
-    # for i in path:
-    #     print(f"{i:04b}")
+    for i in path:
+        print(f"{i:04b}")
+
     print("---------------------")
     print("DP bottom up")
-    result, dp_path = mwis_bottom_up(board, N_CARDS)
+    result, path = mwis_bottom_up(board, N_CARDS)
     print(f"Result: {result}")
-    # for i in path:
-    #     print(f"{i:04b}")
+    for i in path:
+        print(f"{i:04b}")
+
     print("---------------------")
     print("Greedy with local repair")
     result, path = greedy_and_repair(board, N_CARDS)
     print(f"Total sum: {result}\n")
 
-    # print("---------------------")
-    # print("Genetic algorithm")
-    # result = genetic_algorithm.GeneticAlgorithm(
-    #     q.q,
-    #     mutation.mutation,
-    #     reproduction.reproduction,
-    #     crossover.crossover,
-    #     succession.elitism,
-    #     population_count=150,
-    #     probability_of_crossover=0.99,
-    #     probability_of_mutation=0.01,
-    #     fes=20000,
-    #     num_of_cards=N_CARDS,
-    #     board=board,
-    #     num_of_best_survivors=2,
-    # ).run()
-    # print("Result:", result[1])
-    # print("---------------------")
-    # print("Genetic algorithm")
-    # result = genetic_algorithm.GeneticAlgorithm(
-    #     q.q,
-    #     mutation.mutation,
-    #     reproduction.reproduction,
-    #     crossover.crossover,
-    #     succession.elitism,
-    #     population_count=150,
-    #     probability_of_crossover=0.99,
-    #     probability_of_mutation=0.01,
-    #     fes=20000,
-    #     num_of_cards=N_CARDS,
-    #     board=board,
-    #     num_of_best_survivors=2,
-    # ).run()
-    # print("Result:", result[1])
+    print("---------------------")
+    print("Genetic algorithm")
+    result, path = genetic_algorithm.GeneticAlgorithm(
+        q.q,
+        mutation.mutation,
+        reproduction.reproduction,
+        crossover.crossover,
+        succession.elitism,
+        population_count=150,
+        probability_of_crossover=0.99,
+        probability_of_mutation=0.01,
+        fes=20000,
+        num_of_cards=N_CARDS,
+        board=board,
+        num_of_best_survivors=2,
+    ).run()
+    print("Result:", result)
+    for i in path:
+        print(f"{i:04b}")
 
     print("---------------------")
     print("A*")
-    result, a_path = AStar(board, N_CARDS).run()
+    result, path = AStar(board, N_CARDS).run()
     print("Result:", result)
-    for a, dp in zip(a_path, dp_path):
-        assert a == dp
+    for i in path:
+        print(f"{i:04b}")
 
 
 if __name__ == "__main__":
