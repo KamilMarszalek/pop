1d8fb3ce123be5258ff4612867f18ad5c08c4560
KamilMarszalek <kamilmar553@gmail.com>
2026-01-22 13:20:26 +0100

    feat: add result generation for non-deterministic experiments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from src.experiment.config import PHASES, AlgorithmConfig
+from src.experiment.non_deterministic_result_maker import NonDeterministicResultMaker
 from src.experiment.runner import ExperimentRunner, RunnerConfig
 
 SEED = 42
@@ -14,6 +15,17 @@ def main() -> None:
         )
         runner = ExperimentRunner(runner_config)
         runner.run_parallel()
+    for phase in PHASES:
+        nondeterministic_csv_path = (
+            Path("results") / Path(f"{phase.name}") / Path("tables/greedy.csv")
+        )
+        deterministic_csv_path = Path("results") / Path(f"{phase.name}") / Path("tables/astar.csv")
+        output_csv_path = Path("results") / Path(f"{phase.name}") / Path("tables/greedy.csv")
+
+        maker = NonDeterministicResultMaker(
+            nondeterministic_csv_path, deterministic_csv_path, output_csv_path
+        )
+        maker.make_results()
 
 
 if __name__ == "__main__":
